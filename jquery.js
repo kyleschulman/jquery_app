82a10319788ac50b1faa183c423a62b61f420fe9
Kyle Schulman <kyleschulman@Kyle-MBP.local>
2020-03-19 12:53:01 -0700

    s

--- jquery.js
+++ jquery.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 "In China, there have been 35,713 cases and 2,978 deaths."
 "In Washington, there have been 1,012 cases and 52 deaths."
 "In New York, there have been 2,383 cases and 20 deaths."
